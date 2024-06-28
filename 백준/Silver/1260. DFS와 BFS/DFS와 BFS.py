9634a08fcb3602727bb2b948af6488f7f0e09c22
노수진(Sujin Roh) <suucong2@swu.ac.kr>
2024-06-28 18:17:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 460 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,20 +1,23 @@
 from collections import deque
 
 N, M, V = map(int, input().split())
-G = [[0] * (N + 1) for _ in range(N + 1)]
+G = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     u, v = map(int, input().split())
     # 양방향 그래프 처리
-    G[u][v] = 1
-    G[v][u] = 1
+    G[u].append(v)
+    G[v].append(u)
+
+for i in range(N + 1):
+    G[i].sort()
 
 def DFS(V, visited = [0] * (N + 1)):
     visited[V] = 1
     print(V, end=" ")
 
-    for i in range(1, N + 1):
-        if visited[i] == 0 and G[V][i] == 1:
+    for i in G[V]:
+        if visited[i] == 0:
             DFS(i, visited)
 
 def BFS(V):
@@ -25,8 +28,8 @@ def BFS(V):
     while queue:
         v = queue.popleft()
         print(v, end=" ")
-        for i in range(N + 1):
-            if visited[i] == 0 and G[v][i] == 1:
+        for i in G[v]:
+            if visited[i] == 0:
                 queue.append(i)
                 visited[i] = 1
 
