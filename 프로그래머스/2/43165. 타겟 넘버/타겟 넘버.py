859e10de71f3a2d9cbd3d656f433f1d0bebb2775
노수진(Sujin Roh) <suucong2@swu.ac.kr>
2025-07-27 19:31:27 +0900

    [level 2] Title: 타겟 넘버, Time: 636.50 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,18 @@
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
+    q = deque()
+    q.append((0, 0))
+    
+    while q:
+        current_sum, i = q.popleft()
+        
+        if i == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            q.append((current_sum + numbers[i], i + 1))
+            q.append((current_sum - numbers[i], i + 1))
+    
     return answer
