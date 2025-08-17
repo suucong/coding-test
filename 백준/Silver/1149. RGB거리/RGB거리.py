01b34120cf9f0aa7eb931dec4c92bb3b3cb6ab8e
노수진(Sujin Roh) <suucong2@swu.ac.kr>
2025-08-17 17:42:32 +0900

    [Silver I] Title: RGB거리, Time: 60 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,15 @@
+n = int(input())
+costs = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * 3 for _ in range(n)]
+
+dp[0][0] = costs[0][0]  # R
+dp[0][1] = costs[0][1]  # G
+dp[0][2] = costs[0][2]  # B
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
+
+print(min(dp[n-1]))
