18919c7e79e3f1fcd5870ea6b84b597f1ab7642a
Sujin Roh <suucong2@swu.ac.kr>
2025-11-10 18:45:32 +0900

    [Silver V] Title: 단어 정렬, Time: 360 ms, Memory: 25916 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,32 +1,31 @@
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // 입력
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String[] list = new String[n];
-        for (int i = 0; i < n; i++) list[i] = scanner.next();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 구현
-        list = Arrays.stream(list).distinct().toArray(String[]::new);   // 중복 제거
-        int j;
-        for (int i = 1; i < list.length; i++) {
-            String target = list[i];
-            for (j = i - 1; j >= 0; j--) {
-                if (target.length() < list[j].length()) {
-                    list[j + 1] = list[j];
-                } else if (target.length() == list[j].length()) {
-                    if (target.compareTo(list[j]) < 0)  list[j + 1] = list[j];
-                    else break;
-                } else break;
-            }
-            list[j + 1] = target;
+        int n = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
         }
 
-        // 출력
-        for (String str : list) System.out.println(str);
-    }
+        List<String> list = new ArrayList<>(set);
+
+        Collections.sort(list, (a, b) -> {
+            if (a.length() != b.length()) return a.length() - b.length();   // 길이 기준
+            return a.compareTo(b);  // 사전 순
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : list) {
+            sb.append(word).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    } 
 }
+
+
