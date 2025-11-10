2fad165f2c6b640322e95adc99ad4f5c8d9b8209
Sujin Roh <suucong2@swu.ac.kr>
2025-11-10 21:21:21 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 44756 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a  = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if (binarySearch(a, target)) sb.append("1\n");
+            else sb.append("0\n");
+        }
+
+        System.out.println(sb.toString());
+    } 
+
+    static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (arr[mid] == target) return true;
+            else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+}
